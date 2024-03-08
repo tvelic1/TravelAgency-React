c665c7fa75598503c2999649ba6ea691637d5da7
tvelic1 <tvelic1@etf.unsa.ba>
2024-03-08 22:11:37 +0100

    baza i login

--- backend/index.js
+++ backend/index.js
@@ -29,6 +29,45 @@ app.get('/amka', (req, res) => {
         res.json(result);
     });
 });
+app.post('/register', (req, res) => {
+    const { ime, prezime, username, password, email, daLiJeAdmin, spol } = req.body;
+    
+    // Provjera da li postoji korisnik s istim korisničkim imenom
+    const checkQuery = "SELECT * FROM korisnik WHERE username = ?";
+    db.query(checkQuery, [username], (checkError, checkResults, checkFields) => {
+        if (checkError) {
+            res.status(500).json({ error: checkError.message });
+        } else {
+            if (checkResults.length > 0) {
+                // Korisnik s istim korisničkim imenom već postoji
+                res.status(400).json({ error: 'Korisnik s tim korisničkim imenom već postoji.' });
+            } else {
+                // Dodavanje novog korisnika u bazu
+                const insertQuery = "INSERT INTO korisnik(ime, prezime, username, password, email, daLiJeAdmin, spol) VALUES (?, ?, ?, ?, ?, ?, ?)";
+                db.query(insertQuery, [ime, prezime, username, password, email, daLiJeAdmin, spol], (insertError, insertResults, insertFields) => {
+                    if (insertError) {
+                        res.status(500).json({ error: insertError.message });
+                    } else {
+                        res.status(200).json({ message: 'Uspješno ste se registrirali!' });
+                    }
+                });
+            }
+        }
+    });
+});
+app.post('/login',(req,res)=>{
+    const { username, password } = req.body;
+    db.query("SELECT * FROM korisnik WHERE username = ? and password = ?", [username, password], (error, results, fields) => {
+        if (error) {
+            res.status(500).json({ error: error.message });
+        } else {
+            if (results.length > 0)
+                res.status(200).json({ message: 'Uspješno ste se prijavili!' });
+            else
+                res.status(500).json({ error: "Ne postoji korisnik" });
+        }
+    });
+});
 const PORT = 4000;
 app.listen(PORT, () => {
     console.log(`Running on ${PORT}`);
