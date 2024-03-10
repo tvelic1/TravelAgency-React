48fc4dd807b323e12ab4e780e29f58282c74e537
tvelic1 <tvelic1@etf.unsa.ba>
2024-03-10 18:26:05 +0100

    baza dodana i sve funkc

--- backend/index.js
+++ backend/index.js
@@ -3,9 +3,18 @@ const bp =require('body-parser')
 const app=express()
 const mysql=require('mysql')
 const cors=require('cors')
+const session = require('express-session');
+
+app.use(session({
+  secret: 'tajna', // Tajni ključ za potpisivanje ID-a sesije, promijenite u stvarnu tajnu
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: false } // Postavite na true ako služite stranice preko HTTPS
+}));
+
 app.use(bp.urlencoded({extended: false}));
 app.use(bp.json());
-app.use(cors())
+app.use(cors());
 const db=mysql.createConnection({
     host:"localhost",
     user:"root",
@@ -19,14 +28,34 @@ db.connect((err) => {
     console.log("Connected to the database successfully.");
 });
 
-app.get('/amka', (req, res) => {
-    db.query("SELECT * FROM user", (err, result) => {
+app.get('/destinacije', (req, res) => {
+    db.query("SELECT * FROM destinacija", (err, result) => {
         if (err) {
             // Ako dođe do greške, šaljemo status 500 (Internal Server Error) i poruku o grešci
             return res.status(500).send(err);
         }
         // Ako nema greške, šaljemo dobijene podatke koristeći res.json()
         res.json(result);
+    });
+});
+app.post('/res', (req, res) => {
+    db.query("SELECT u.ime, u.prezime, d.naziv AS destination_name FROM rezervacija p JOIN korisnik u ON p.korisnikId = u.id JOIN destinacija d ON p.destinacijaId = d.id WHERE p.korisnikId = ? and p.destinacijaId = ?",[req.body.ajdi,req.body.id], (err, result) => {
+        if (err) {
+            // Ako dođe do greške, šaljemo status 500 (Internal Server Error) i poruku o grešci
+            return res.status(500).send(err);
+        }
+        // Ako nema greške, šaljemo dobijene podatke koristeći res.json()
+        res.json(result);
+    });
+});
+app.post('/prikazi', (req, res) => {
+    db.query("SELECT p.id, u.ime, u.prezime, d.naziv AS destination_name FROM rezervacija p JOIN korisnik u ON p.korisnikId = u.id JOIN destinacija d ON p.destinacijaId = d.id WHERE p.korisnikId = ?",[req.body.ajdi], (err, result) => {
+        if (err) {
+            // Ako dođe do greške, šaljemo status 500 (Internal Server Error) i poruku o grešci
+            return res.status(500).send(err);
+        }
+        // Ako nema greške, šaljemo dobijene podatke koristeći res.json()
+        res.status(200).json(result);
     });
 });
 app.post('/register', (req, res) => {
@@ -61,10 +90,26 @@ app.post('/login',(req,res)=>{
         if (error) {
             res.status(500).json({ error: error.message });
         } else {
-            if (results.length > 0)
-                res.status(200).json(results);
+            if (results.length > 0) {
+                req.session.user = results[0]; // Sprema korisnika u sesiju
+                req.session.save(() => { // Osigurava da se sesija spremi prije slanja odgovora
+                    res.status(200).json(results[0]);
+                });}
             else
                 res.status(500).json({ error: "Ne postoji korisnik" });
+        }
+    });
+});
+
+app.post('/rezervisi',(req,res)=>{
+    const{id,ajdi}=req.body;
+    db.query("INSERT INTO rezervacija (korisnikId, destinacijaId) VALUES (?, ?)", [ajdi, id], (error, results, fields) => {
+        if (error) {
+            console.log(req.session.user.id)
+
+            res.status(500).json({ error: "Error" });
+        } else {
+           res.status(200).json({message: "Uspješno dodana rezervacija"});
         }
     });
 });
